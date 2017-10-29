41b1fcd11bcb37c186a3deb5245574db9fa80094
Unknown <docroflcopter@gmail.com>
2017-10-28 20:28:49 -0400

    Initial commit : Base app

---
+++ YoutubeDL/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YoutubeDL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        string selectedDirectory;
+        string youtubeDlLocation;
+        string youtubeURL;
+        Process cmd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (cmd != null && cmd.HasExited != true)
+            {
+                cmd.CancelErrorRead();
+                cmd.CancelOutputRead();
+                cmd.Close();
+                cmd.WaitForExit();
+            }
+        }
+
+        private void clickSetDownloadDirectory(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                selectedDirectory = dialog.FileName;
+                directorySelect.Content = dialog.FileName;
+            }
+        }
+
+        private void clickSetYoutubeDLDirectory(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                youtubeDlLocation = dialog.FileName;
+                youtubeLocation.Content = dialog.FileName;
+            }
+        }
+
+        private void clickGetAudio(object sender, RoutedEventArgs e)
+        {
+            youtubeURL = txtYoutubeURL.Text;
+            getMedia(youtubeURL, selectedDirectory, youtubeDlLocation, "140");
+        }
+
+        private void clickGetVideo(object sender, RoutedEventArgs e)
+        {
+            youtubeURL = txtYoutubeURL.Text;
+            getMedia(youtubeURL, selectedDirectory, youtubeDlLocation, "best");
+        }
+
+        void cmdOutputRecieved(object sender, DataReceivedEventArgs e)
+        {
+            updateStatus(e.Data);
+        }
+
+        void cmdErrorReceived(object sender, DataReceivedEventArgs e)
+        {
+            updateStatus(e.Data);
+        }
+
+        void cmdExit(object sender, EventArgs e)
+        {
+            cmd.OutputDataReceived -= new DataReceivedEventHandler(cmdOutputRecieved);
+            cmd.Exited -= new EventHandler(cmdExit);
+        }
+
+        public void getMedia(string youtubeURL, string targetDirectory, string youtubeDLLocation, string typeWanted)
+        {
+            statusConsole.Text = "";
+
+            string gotoTargetDirectory = "cd " + targetDirectory;
+            string convertCommandAudio = youtubeDLLocation + " -f " + typeWanted + " " + youtubeURL;
+
+            ProcessStartInfo pStart = new ProcessStartInfo();
+            pStart.FileName = "cmd";
+            pStart.WorkingDirectory = targetDirectory;
+            pStart.Arguments = "/K " + convertCommandAudio;
+            pStart.RedirectStandardInput = true;
+            pStart.RedirectStandardOutput = true;
+            pStart.RedirectStandardError = true;
+            pStart.UseShellExecute = false;
+            pStart.CreateNoWindow = true;
+            pStart.WindowStyle = ProcessWindowStyle.Hidden;
+
+            cmd = new Process();
+            cmd.StartInfo = pStart;
+
+            if (cmd.Start() == true)
+            {
+                cmd.OutputDataReceived += new DataReceivedEventHandler(cmdOutputRecieved);
+                cmd.ErrorDataReceived += new DataReceivedEventHandler(cmdErrorReceived);
+                cmd.Exited += new EventHandler(cmdExit);
+
+                cmd.BeginOutputReadLine();
+                cmd.BeginErrorReadLine();
+            }
+            else
+            {
+                cmd = null;
+            }
+        }
+
+        private void updateStatus(string text)
+        {
+            if (!statusConsole.Dispatcher.CheckAccess())
+            {
+                statusConsole.Dispatcher.Invoke(new Action(()
+                    => { writeToConsole(text); }));
+            }
+            else
+            {
+                writeToConsole(text);
+            }
+        }
+
+        private void writeToConsole(string text)
+        {
+            if (text != null)
+            {
+                Span line = new Span();
+                foreach (string textLine in text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                {
+                    line.Inlines.Add(new Run(textLine));
+                }
+                line.Inlines.Add(new LineBreak());
+                statusConsole.Inlines.Add(line);
+            }
+        }
+
+        private void ScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            statusScroll.ScrollToBottom();
+        }
+    }
+}
